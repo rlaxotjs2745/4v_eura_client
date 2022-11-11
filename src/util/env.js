d745faaf10dc654ffe959ecf7eeb09e51acf9623
4thevisionDonghyun <donghyun@4thevision.com>
2022-11-11 15:06:02 +0900

    221111 오후 3시 업데이트

--- src/util/env.js
+++ src/util/env.js
@@ -2,7 +2,7 @@
 export const SERVER_URL = "http://192.168.0.7:10000"
 
 // 팀장님 제공 api 테스트 서버
-export const SERVER_URL2 = "http://192.168.0.7:10000"
+// export const SERVER_URL2 = "http://192.168.0.7:10000"
 
 export const AXIOS_OPTION = {
     withCredentials: true
