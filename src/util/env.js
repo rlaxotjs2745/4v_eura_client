eca956d7d5cf3a16076ba481ae9047bc1a077b4b
teaseon <taeseon@4thevision.com>
2022-11-11 18:26:07 +0900

    server url 수정 및 포트 80 변경

--- src/util/env.js
+++ src/util/env.js
@@ -1,6 +1,6 @@
 // 은진 매니저님 제공 api 테스트 서버
 
-export const SERVER_URL = "http://api.eura.site:10000"
+export const SERVER_URL = "http://api.eura.site"
 
 // 팀장님 제공 api 테스트 서버
 // export const SERVER_URL2 = "http://192.168.0.7:10000"
