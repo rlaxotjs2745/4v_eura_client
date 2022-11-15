8d1338aa50c6a58c79825933174439e0ef8e02a0
4thevisionDonghyun <donghyun@4thevision.com>
2022-11-15 15:30:19 +0900

    로그인 로그아웃 관련 수정

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
         getCookie('user_id')
         // console.log(getCookie('user_id'))
         removeCookie('user_id'); // 쿠키를 삭제
-        document.cookie = 'user_id' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.eura.site;path=/;';
+        // document.cookie = 'user_id' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.eura.site;path=/;';
         navigate('/login'); // 로그인 페이지로 이동
     };
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -105,14 +105,14 @@ const Login = () => {
                 if(e.autoLogin) {
                     console.log(e.autoLogin)
                     tomorrow.setDate(today.getDate()+30);
-                    // document.cookie = `user_id=${inputId}; expires=${tomorrow};`;
+                    document.cookie = `user_id=${inputId}; expires=${tomorrow};`;
                     // setCookie('user_id', inputId, {path:'/', expires:tomorrow});
                 }
                     // 자동로그인 체크 안했으면 쿠키 하루
                     else if (!e.autoLogin) {
                     console.log(e.autoLogin)
                     tomorrow.setDate(today.getDate()+1);
-                    // document.cookie = `user_id=${inputId}; expires=${tomorrow};`;
+                    document.cookie = `user_id=${inputId}; expires=${tomorrow};`;
                     // setCookie('user_id', inputId, {path:'/', expires:tomorrow});
                 }
                 console.log('---------ID', inputId)
@@ -130,13 +130,13 @@ const Login = () => {
                 // 자동로그인 체크 했으면 쿠키 30일
                 if(inputChk) {
                     tomorrow.setDate(today.getDate()+30);
-                    // document.cookie = `user_id=${inputId}; expires=${tomorrow};`
+                    document.cookie = `user_id=${inputId}; expires=${tomorrow};`
                     // setCookie('user_id', document.cookie, {path:'/', expires:tomorrow});
                 }
                 // 자동로그인 체크 안했으면 쿠키 하루
                 else if (!inputChk) {
                     tomorrow.setDate(today.getDate()+1);
-                    // document.cookie = `user_id=${inputId}; expires=${tomorrow};`
+                    document.cookie = `user_id=${inputId}; expires=${tomorrow};`
                     // setCookie('user_id', inputId, {path:'/', expires:tomorrow});
                 }
                 console.log('======================',res.data.result_str);
