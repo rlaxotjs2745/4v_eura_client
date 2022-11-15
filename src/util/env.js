09f7b13eb112f0b8faa7c44850074270ab21f3ff
4thevisionDonghyun <donghyun@4thevision.com>
2022-11-15 23:25:05 +0900

    미팅룸생성변경

--- src/util/env.js
+++ src/util/env.js
@@ -1,7 +1,7 @@
 // 은진 매니저님 제공 api 테스트 서버
 
-// export const SERVER_URL = "http://api.eura.site"
-export const SERVER_URL = "http://localhost:10000"
+export const SERVER_URL = "http://api.eura.site"
+// export const SERVER_URL = "http://localhost:10000"
 
 // 팀장님 제공 api 테스트 서버
 // export const SERVER_URL = "http://192.168.0.9:10000"
@@ -20,5 +20,5 @@ export const AXIOS_FORM_DATA_OPTION = {
     withCredentials: true
 }
 
-// export const COOKIE_DOMAIN = "eura.site";
-export const COOKIE_DOMAIN = "localhost";
+export const COOKIE_DOMAIN = "eura.site";
+// export const COOKIE_DOMAIN = "localhost";
