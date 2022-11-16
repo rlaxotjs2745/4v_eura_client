86f20708cb16fcab06d8f783528daa1c2227b878
teaseon <taeseon@4thevision.com>
2022-11-16 17:55:42 +0900

    remove env Set-Cookie

--- src/util/env.js
+++ src/util/env.js
@@ -1,7 +1,7 @@
 // 은진 매니저님 제공 api 테스트 서버
 import {getCookie} from "./cookie";
 
-// export const SERVER_URL = "https://api.eura.site"
+// export const SERVER_URL = "https" + "://api.eura.site"
 export const SERVER_URL = "http://localhost:10000"
 
 // 팀장님 제공 api 테스트 서버
@@ -9,16 +9,16 @@ export const SERVER_URL = "http://localhost:10000"
 
 export const AXIOS_OPTION = {
     headers: {
-        "auth":getCookie('user_id')
+        "auth": getCookie('user_id'),
     }
 };
 
 export const AXIOS_FORM_DATA_OPTION = {
     headers: {
-    "Content-Type": "multipart/form-data",
-        "auth":getCookie('user_id')
+        "Content-Type": "multipart/form-data",
+        "auth": getCookie('user_id')
     }
 }
 
-// export const COOKIE_DOMAIN = "eura.site";
-export const COOKIE_DOMAIN = "localhost";
+export const COOKIE_DOMAIN = "eura.site";
+// export const COOKIE_DOMAIN = "localhost";
