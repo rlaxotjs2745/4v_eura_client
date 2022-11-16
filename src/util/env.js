50182f834ba86ef18e14631f199186f2f970b727
WiNMasTeR-SW <sungwoong@4thevision.com>
2022-11-16 21:05:16 +0900

    Update env.js

--- src/util/env.js
+++ src/util/env.js
@@ -1,8 +1,8 @@
 // 은진 매니저님 제공 api 테스트 서버
 import {getCookie} from "./cookie";
 
-// export const SERVER_URL = "https://api.eura.site"
-export const SERVER_URL = "http://localhost:10000"
+export const SERVER_URL = "https://api.eura.site"
+// export const SERVER_URL = "http://localhost:10000"
 
 // 팀장님 제공 api 테스트 서버
 // export const SERVER_URL = "http://192.168.0.9:10000"
@@ -21,5 +21,5 @@ export const AXIOS_FORM_DATA_OPTION = {
     }
 }
 
-// export const COOKIE_DOMAIN = "eura.site";
-export const COOKIE_DOMAIN = "localhost";
+export const COOKIE_DOMAIN = "eura.site";
+// export const COOKIE_DOMAIN = "localhost";
