4a1275a1aad5be6da3e2152e0282e012d06c8f75
teaseon <taeseon@4thevision.com>
2022-11-21 19:52:32 +0900

    meetingroom live 시 유저 집중도에 따른 색상 변경

--- src/Components/Cards/RoomUser.js
+++ src/Components/Cards/RoomUser.js
@@ -9,7 +9,8 @@ const {useState} = require("react");
 
 const RoomUser = ({inv, roomInfo}) => {
         return inv.is_host ? null :
-            <li className={roomInfo.mt_live ? 'user' : 'user is-disabled'}>
+            !roomInfo.mt_live ?
+            <li className='user is-disabled'>
                     <>
                             <figure><img
                                 src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
@@ -17,7 +18,47 @@ const RoomUser = ({inv, roomInfo}) => {
                             <span className="team__user">{inv.uname}<em>{inv.email}</em></span>
                             {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
                     </>
-            </li>
+            </li> :
+                roomInfo.mt_live && inv.is_status === 2 ?
+                    <li className='user hilight__normal'>
+                        <>
+                            <figure><img
+                                src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
+                                alt=""/></figure>
+                            <span className="team__user">{inv.uname}<em>{inv.email}</em></span>
+                            {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
+                        </>
+                    </li> :
+                    roomInfo.mt_live && inv.is_status === 3 ?
+                        <li className='user hilight__bad'>
+                            <>
+                                <figure><img
+                                    src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
+                                    alt=""/></figure>
+                                <span className="team__user">{inv.uname}<em>{inv.email}</em></span>
+                                {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
+                            </>
+                        </li> :
+                        roomInfo.mt_live && inv.is_status === 4 ?
+                            <li className='user'>
+                                <>
+                                    <figure><img
+                                        src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
+                                        alt=""/></figure>
+                                    <span className="team__user">{inv.uname}<em>{inv.email}</em></span>
+                                    <span className="badge__camera"><img src={require('../../assets/image/ic_camera_off.png')} alt="" /></span>
+                                    {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
+                                </>
+                            </li> :
+                            <li className='user'>
+                                <>
+                                    <figure><img
+                                        src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
+                                        alt=""/></figure>
+                                    <span className="team__user">{inv.uname}<em>{inv.email}</em></span>
+                                    {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
+                                </>
+                            </li>
 }
 
 
