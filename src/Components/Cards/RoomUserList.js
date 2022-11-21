eaeda199f3c307177fd59e362df5763ece94dfac
teaseon <taeseon@4thevision.com>
2022-11-21 16:48:57 +0900

    호스트 상단 고정

--- src/Components/Cards/RoomUserList.js
+++ src/Components/Cards/RoomUserList.js
@@ -18,14 +18,15 @@ const RoomUserList = ({invites, idx, roomInfo}) =>
                     {
                         invites.map(inv =>
                             inv.is_host ?
-                            <>
-                                <figure><img
-                                    src={inv.picture ? inv.picture : require('../../assets/image/image_profile.png')}
-                                    alt=""/></figure>
-                                <span className="team__user">{inv.uname}<img
-                                    src={require('../../assets/image/ic_host.png')} alt=""/><em>{inv.email}</em></span>
-                                {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
-                            </> : '')
+
+                                <li className={roomInfo.mt_live ? 'user' : 'user is-disabled'}>
+                                    <figure><img
+                                        src={inv.picture ? inv.picture : require('../../assets/image/image_profile.png')}
+                                        alt=""/></figure>
+                                    <span className="team__user">{inv.uname}<img
+                                        src={require('../../assets/image/ic_host.png')} alt=""/><em>{inv.email}</em></span>
+                                    {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
+                                </li> : '')
                     }
                     {
                         invites.map(inv => <RoomUser inv={inv} roomInfo={roomInfo} />)
