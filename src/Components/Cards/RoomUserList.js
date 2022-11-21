aa1a900c3c5da50d9e8e165d19fabae131a67cf5
teaseon <taeseon@4thevision.com>
2022-11-21 18:01:30 +0900

    user card 수정

--- src/Components/Cards/RoomUserList.js
+++ src/Components/Cards/RoomUserList.js
@@ -19,13 +19,15 @@ const RoomUserList = ({invites, idx, roomInfo}) =>
                         invites.map(inv =>
                             inv.is_host ?
                                 <li className={roomInfo.mt_live ? 'user' : 'user is-disabled'}>
+                                    <>
                                     <figure><img
-                                        src={inv.picture ? inv.picture : require('../../assets/image/image_profile.png')}
+                                        src={inv.ui_pic ? inv.ui_pic : require('../../assets/image/image_profile.png')}
                                         alt=""/></figure>
                                     <span className="team__user">{inv.uname}<img
                                         src={require('../../assets/image/ic_host.png')} alt=""/><em>{inv.email}</em></span>
                                     {!!inv.is_iam ? <span className="team__badge">나</span> : ''}
-                                </li> : '')
+                                    </>
+                                </li> : null)
                     }
                     {
                         invites.map(inv => <RoomUser inv={inv} roomInfo={roomInfo} />)
