d3279ff56c1b00f7fe281038cdcd8cdbb829410e
teaseon <taeseon@4thevision.com>
2022-11-24 10:57:34 +0900

    oneUserGraph 이미지 넣기 성공

--- src/Components/Cards/InviteMyAnalPieGraphCard.js
+++ src/Components/Cards/InviteMyAnalPieGraphCard.js
@@ -44,7 +44,6 @@ const renderActiveShape = props => {
         <g>
             <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
                 {/*{payload.name}*/}
-                <div><img className="pieImage" src={require('../../assets/image/icon_angry.png')}/></div>
             </text>
             <Sector
                 cx={cx}
@@ -55,31 +54,6 @@ const renderActiveShape = props => {
                 endAngle={endAngle}
                 fill={fill}
             />
-            {payload.name !== "No Data Found" && (
-                <React.Fragment>
-                    <path
-                        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-                        stroke={fill}
-                        fill="none"
-                    />
-                    <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-                    <text
-                        x={ex + (cos >= 0 ? 1 : -1) * 12}
-                        y={ey}
-                        textAnchor={textAnchor}
-                        fill="#333"
-                    >{`PV ${value}`}</text>
-                    <text
-                        x={ex + (cos >= 0 ? 1 : -1) * 12}
-                        y={ey}
-                        dy={18}
-                        textAnchor={textAnchor}
-                        fill="#999"
-                    >
-                        {`(Rate ${(percent * 100).toFixed(2)}%)`}
-                    </text>
-                </React.Fragment>
-            )}
         </g>
     );
 };
@@ -87,36 +61,33 @@ const renderActiveShape = props => {
 const InviteMyAnalPieGraphCard = () => {
     const [initialState, setInitialState] = useState({activeIndex: 0});
     const data = [
-        { name: "Group A", value: 400 },
-        { name: "Group B", value: 300 },
-        { name: "Group C", value: 300 },
-        { name: "Group D", value: 200 }
+        { name: "Good", value: 700 },
+        { name: "Bad", value: 200 },
+        { name: "Camera Off", value: 100 },
     ];
-    const data1 = [
-        { name: "No Data Found", value: 1 },
-        { name: "No Data Found", value: 1 },
-        { name: "No Data Found", value: 1 }
-    ];
-    const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+    const COLORS = ['#3377ff', '#ffc633', 'gray'];
 
             return (
                 <div className="result__one_user">
-                    <PieChart width={400} height={400}>
-                        <Pie
-                            activeIndex={initialState.activeIndex}
-                            activeShape={renderActiveShape}
-                            data={data}
-                            cx={300}
-                            cy={200}
-                            innerRadius={60}
-                            outerRadius={80}
-                            // onMouseEnter={this.onPieEnter}
-                        >
-                            {data.map((entry, index) => (
-                                <Cell key={`cell-${index}`} fill={colors[index]} />
-                            ))}
-                        </Pie>
-                    </PieChart>
+                    <div className="one_user_graph_image">
+                        <img className="pieImage" src={require('../../assets/image/icon_angry.png')}/>
+                    </div>
+                    <div className="one_user_graph">
+                        <PieChart width={200} height={200}>
+                            <Pie
+                                activeIndex={initialState.activeIndex}
+                                activeShape={renderActiveShape}
+                                data={data}
+                                innerRadius={80}
+                                outerRadius={100}
+                                // onMouseEnter={this.onPieEnter}
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                                ))}
+                            </Pie>
+                        </PieChart>
+                    </div>
                 </div>
             );
 }
