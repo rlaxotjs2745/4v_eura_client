bed09a95cbc4ab0700027ad34cdb57a9e751bd32
4thevisionDonghyun <donghyun@4thevision.com>
2022-11-28 13:56:33 +0900

    프로필 비밀번호 변경시 쿠키 삭제 .eura.site

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -202,6 +202,7 @@ const Profile = () => {
 
     const logOut = () => {
         removeCookie('user_id'); // 쿠키를 삭제
+        document.cookie = 'user_id' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.eura.site;path=/;'; //임시 도메인 날리기 (서버에서 보내주는 쿠키 삭제용)
         navigate('/login'); // 로그인 페이지로 이동
     };
 
