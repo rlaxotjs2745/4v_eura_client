ed9e2beb5c430808b6dd267cb9420301f3ddba60
4thevisionDonghyun <donghyun@4thevision.com>
2022-11-30 11:34:47 +0900

    미팅룸 되풀이 미팅 주기 기획서 미반영 으로 인해 변경

--- src/pages/NewRoom.js
+++ src/pages/NewRoom.js
@@ -780,6 +780,7 @@ const NewRoom = () => {
                                     <dd>
                                         {
                                             selectValue === 1 ?
+                                                <>
                                                 <select name="" id="room_repeat2" onChange={selectRemindCount} className="make-select" defaultValue={weekday}>
                                                     <option value="1">1</option>
                                                     <option value="2">2</option>
@@ -811,22 +812,114 @@ const NewRoom = () => {
                                                     <option value="28">28</option>
                                                     <option value="29">29</option>
                                                     <option value="30">30</option>
+                                                    <option value="31">31</option>
+                                                    <option value="32">32</option>
+                                                    <option value="33">33</option>
+                                                    <option value="34">34</option>
+                                                    <option value="35">35</option>
+                                                    <option value="36">36</option>
+                                                    <option value="37">37</option>
+                                                    <option value="38">38</option>
+                                                    <option value="39">39</option>
+                                                    <option value="40">40</option>
+                                                    <option value="41">41</option>
+                                                    <option value="42">42</option>
+                                                    <option value="43">43</option>
+                                                    <option value="44">44</option>
+                                                    <option value="45">45</option>
+                                                    <option value="46">46</option>
+                                                    <option value="47">47</option>
+                                                    <option value="48">48</option>
+                                                    <option value="49">49</option>
+                                                    <option value="50">50</option>
+                                                    <option value="51">51</option>
+                                                    <option value="52">52</option>
+                                                    <option value="53">53</option>
+                                                    <option value="54">54</option>
+                                                    <option value="55">55</option>
+                                                    <option value="56">56</option>
+                                                    <option value="57">57</option>
+                                                    <option value="58">58</option>
+                                                    <option value="59">59</option>
+                                                    <option value="60">60</option>
+                                                    <option value="61">61</option>
+                                                    <option value="62">62</option>
+                                                    <option value="63">63</option>
+                                                    <option value="64">64</option>
+                                                    <option value="65">65</option>
+                                                    <option value="66">66</option>
+                                                    <option value="67">67</option>
+                                                    <option value="68">68</option>
+                                                    <option value="69">69</option>
+                                                    <option value="70">70</option>
+                                                    <option value="71">71</option>
+                                                    <option value="72">72</option>
+                                                    <option value="73">73</option>
+                                                    <option value="74">74</option>
+                                                    <option value="75">75</option>
+                                                    <option value="76">76</option>
+                                                    <option value="77">77</option>
+                                                    <option value="78">78</option>
+                                                    <option value="79">79</option>
+                                                    <option value="80">80</option>
+                                                    <option value="81">81</option>
+                                                    <option value="82">82</option>
+                                                    <option value="83">83</option>
+                                                    <option value="84">84</option>
+                                                    <option value="85">85</option>
+                                                    <option value="86">86</option>
+                                                    <option value="87">87</option>
+                                                    <option value="88">88</option>
+                                                    <option value="89">89</option>
+                                                    <option value="90">90</option>
                                                 </select>
-                                                :
-                                                <select name="" id="room_repeat2" onChange={selectRemindCount} className="make-select">
-                                                    <option value="1">1</option>
-                                                    <option value="2">2</option>
-                                                    <option value="3">3</option>
-                                                    <option value="4">4</option>
-                                                </select>
-
-                                        }
-                                        {
-                                            selectValue === 1 ? '일'
-                                                : selectValue === 2 ? '주'
-                                                    : selectValue === 3 ? '개월'
-                                                        : selectValue === 4 ? '년'
-                                                            : '일'
+                                                    일
+                                                </>
+                                                : selectValue === 2 ?
+                                                    <>
+                                                        <select name="" id="room_repeat2" onChange={selectRemindCount} className="make-select">
+                                                            <option value="1">1</option>
+                                                            <option value="2">2</option>
+                                                            <option value="3">3</option>
+                                                            <option value="4">4</option>
+                                                            <option value="5">5</option>
+                                                            <option value="6">6</option>
+                                                            <option value="7">7</option>
+                                                            <option value="8">8</option>
+                                                            <option value="9">9</option>
+                                                            <option value="10">10</option>
+                                                            <option value="11">11</option>
+                                                            <option value="12">12</option>
+                                                        </select>주
+                                                    </>
+                                                    : selectValue === 3 ?
+                                                        <>
+                                                            <select name="" id="room_repeat2" onChange={selectRemindCount} className="make-select">
+                                                                <option value="1">1</option>
+                                                                <option value="2">2</option>
+                                                                <option value="3">3</option>
+                                                                <option value="4">4</option>
+                                                                <option value="5">5</option>
+                                                                <option value="6">6</option>
+                                                                <option value="7">7</option>
+                                                                <option value="8">8</option>
+                                                                <option value="9">9</option>
+                                                                <option value="10">10</option>
+                                                                <option value="11">11</option>
+                                                                <option value="12">12</option>
+                                                            </select>월
+                                                        </>
+                                                        : selectValue === 4 ?
+                                                            <>
+                                                                <select name="" id="room_repeat2" onChange={selectRemindCount} className="make-select">
+                                                                    <option value="1">1</option>
+                                                                    <option value="2">2</option>
+                                                                    <option value="3">3</option>
+                                                                    <option value="4">4</option>
+                                                                    <option value="5">5</option>
+                                                                </select>년
+                                                            </>
+                                                : ''
                                         }
                                     </dd>
                                 </dl>
